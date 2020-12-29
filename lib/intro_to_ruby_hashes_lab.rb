3cec5b39b0076799da896449da18dd64cfa4e314
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-29 17:22:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def my_hash_creator(key = :name, value = 'Grace Hopper')
   { key => value}
 end  
 
-def read_from_hash(hash = name: 'Steve', key= :name)
+def read_from_hash(hash = {name: 'Steve'}, key= :name)
   hash[key]
 end
 
